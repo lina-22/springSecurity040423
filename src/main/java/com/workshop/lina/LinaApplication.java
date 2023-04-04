c46b6b00163fe7c011dbda14949408da8d8e5131
lina-22 <haquefahinur@gmail.com>
2023-04-04 14:53:47 +0200

    first commit

---
+++ src/main/java/com/workshop/lina/LinaApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.lina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/workshop/lina/controlers/GreetingsController.java
@@ -0,0 +1,23 @@
+package com.workshop.lina.controlers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/V1/greetings")
+public class GreetingsController {
+
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from our API");
+    }
+
+    @GetMapping("/say-good-bye")
+    public ResponseEntity<String> sayGoodBye(){
+        return ResponseEntity.ok("Goodbye and see you later");
+    }
+}
